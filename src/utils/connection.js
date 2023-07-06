491b1a9e1cf13d08633e6fe6e205e7d926d0ff5d
Maricruz Amurrio <mamurrio89@gmail.com>
2023-07-05 22:53:48 -0400

    3rd time

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const database = 'usersfinal_db';
 require('dotenv').config();
-//const sequelize = new Sequelize(process.env.DATABASE_URL)
-const sequelize = new Sequelize(database, null, null, { dialect: 'postgres' });
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+//const sequelize = new Sequelize(database, null, null, { dialect: 'postgres' });
 module.exports = sequelize;
